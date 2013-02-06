5746b92e7bdf3f1df42a39fd99983aa5d6ba963a
Johan Martinsson <martinsson.johan@gmail.com>
2013-02-06 22:15:42 +0100

    added test for no trips when the user has friends but were not part of them

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/TripServiceTest.java
@@ -17,6 +17,7 @@ public class TripServiceTest {
     private static final User FRIEND = new User();
     private static final User GUEST = null;
     private static final User EXISTING_USER = new User();
+    private static final User ANOTHER_USER = new User();
     TripService tripService = new TestableTripService();
 	
     @Test(expected=UserNotLoggedInException.class) public void 
@@ -26,11 +27,24 @@ public class TripServiceTest {
     }
     
     @Test public void 
-    no_trips_when_were_not_friends() throws Exception {
+    no_trips_when_the_user_has_no_friends() throws Exception {
         loggedUser = EXISTING_USER;
         List<Trip> noTrips = Collections.emptyList();
+        expectToSee(noTrips);
+    }
+
+    @Test public void 
+    no_trips_when_were_not_friends_with_the_user() throws Exception {
+         loggedUser = EXISTING_USER;
+         FRIEND.addFriend(ANOTHER_USER);
+         List<Trip> noTrips = Collections.emptyList();
+         expectToSee(noTrips);
+    }
+    
+    private void expectToSee(List<Trip> noTrips) throws UserNotLoggedInException {
         assertThat(tripService.getTripsByUser(FRIEND), equalTo(noTrips));
     }
+    
     public class TestableTripService extends TripService {
 
         @Override
