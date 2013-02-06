d946c77e86f4de79da453483a203863813c11595
Johan Martinsson <martinsson.johan@gmail.com>
2013-02-06 22:09:25 +0100

    added test for no trips as there are no friends

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/TripServiceTest.java
@@ -1,29 +1,41 @@
 package org.craftedsw.tripservicekata;
 
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.*;
 
+import java.util.Collections;
+import java.util.List;
+
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
+import org.craftedsw.tripservicekata.trip.Trip;
 import org.craftedsw.tripservicekata.trip.TripService;
 import org.craftedsw.tripservicekata.user.User;
 import org.junit.Test;
 
 public class TripServiceTest {
-    private User user = new User();
+    private User loggedUser ;
     private static final User FRIEND = new User();
-    private static final User GUEST = new User();
+    private static final User GUEST = null;
+    private static final User EXISTING_USER = new User();
+    TripService tripService = new TestableTripService();
 	
     @Test(expected=UserNotLoggedInException.class) public void 
     throws_exception_for_guests() throws Exception {
-        user = null;
-        TripService tripService = new TestableTripService();
+        loggedUser = GUEST;
         tripService.getTripsByUser(FRIEND);
     }
     
+    @Test public void 
+    no_trips_when_were_not_friends() throws Exception {
+        loggedUser = EXISTING_USER;
+        List<Trip> noTrips = Collections.emptyList();
+        assertThat(tripService.getTripsByUser(FRIEND), equalTo(noTrips));
+    }
     public class TestableTripService extends TripService {
 
         @Override
         protected User loggedUser() {
-            return user;
+            return loggedUser;
         }
     }
 }
