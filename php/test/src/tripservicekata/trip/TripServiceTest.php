82afa160ac759539a6c671cc46050713efd8f952
Johan Martinsson <martinsson.johan@gmail.com>
2013-02-14 09:49:35 +0100

    isolated from loggedInUser

--- php/test/src/tripservicekata/trip/TripServiceTest.php
+++ php/test/src/tripservicekata/trip/TripServiceTest.php
@@ -5,19 +5,31 @@ class TripServiceTest extends PHPUnit_Framework_TestCase {
     /**
      * @var TripService
      */
-    protected $object;
+    private $tripService;
 
     protected function setUp() {
-        $this->object = new TripService;
+        $this->tripService = new TripServiceWODependencies();
     }
 
     /**
+     * @expectedException UserNotLoggedInException
      * @test
      * @covers TripService::getTripsByUser
      */
     public function it_throws_an_exception_if_the_user_is_a_guest() {
-        
-        $this->fail('This test has not been implemented yet.');
+        $this->tripService->getTripsByUser(new User());
     }
+    
+    /**
+     * @test
+     */
+    public function it_doesnt_return_trips_if_the_given_user_has_no_friends() {
+    }
+}
 
+class TripServiceWODependencies extends TripService {
+
+    protected function getLoggedUser() {
+        return null;
+    }
 }
