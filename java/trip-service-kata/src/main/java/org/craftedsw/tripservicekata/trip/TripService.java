e4f83a84b82a69994a3ff5933c1ff71d8c38eca2
Johan Martinsson <martinsson.johan@gmail.com>
2013-09-04 17:06:53 +0200

    covered all the code

--- java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -11,8 +11,9 @@ public class TripService {
 
 	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
 		List<Trip> tripList = new ArrayList<Trip>();
-		User loggedUser = UserSession.getInstance().getLoggedUser();
+		User loggedUser = getLoggedUser();
 		boolean isFriend = false;
+		
 		if (loggedUser != null) {
 			for (User friend : user.getFriends()) {
 				if (friend.equals(loggedUser)) {
@@ -21,12 +22,20 @@ public class TripService {
 				}
 			}
 			if (isFriend) {
-				tripList = TripDAO.findTripsByUser(user);
+				tripList = findTripsByUser(user);
 			}
 			return tripList;
 		} else {
 			throw new UserNotLoggedInException();
 		}
 	}
+
+    public List<Trip> findTripsByUser(User user) {
+        return TripDAO.findTripsByUser(user);
+    }
+
+    public User getLoggedUser() {
+        return UserSession.getInstance().getLoggedUser();
+    }
 	
 }
--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/TripServiceTest.java
@@ -1,5 +1,71 @@
 package org.craftedsw.tripservicekata;
 
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.mockito.Mockito.doReturn;
+
+import java.util.List;
+
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
+import org.craftedsw.tripservicekata.trip.Trip;
+import org.craftedsw.tripservicekata.trip.TripService;
+import org.craftedsw.tripservicekata.user.User;
+import org.junit.Test;
+import org.mockito.Mockito;
+
 public class TripServiceTest {
+    
+    private static final List<Object> NO_TRIPS = asList();
+    private static final User LOGGED_USER = new User();
+    private static final User GUEST = null;
+    private static final Trip TRIP_TO_BAHAMAS = new Trip();
+    private static final Trip TRIP_TO_LONDON = new Trip();
+    TripService service = Mockito.spy(new TripService());
+
+    @Test(expected=UserNotLoggedInException.class) public void 
+    throws_userNotLoggedInException_when_the_viewer_is_guest() throws Exception {
+        Mockito.doReturn(GUEST).when(service).getLoggedUser();
+         
+        User someOne = null;
+        service.getTripsByUser(someOne);
+    }
+    
+    @Test public void 
+    we_get_no_trips_when_the_given_user_has_no_friends() throws Exception {
+        doReturn(LOGGED_USER).when(service).getLoggedUser();
+        User userWithNoFriends = new User();
+        List<Trip> result = service.getTripsByUser(userWithNoFriends );
+        
+        assertEquals(NO_TRIPS, result);;
+    }
+    
+    @Test public void 
+    we_get_no_trips_if_were_not_friends_with_the_given_user() throws Exception {
+        doReturn(LOGGED_USER).when(service).getLoggedUser();
+        User someUser = new User();
+        doReturn(asList()).when(service).findTripsByUser(someUser);
+        User otherUser = new User();
+        someUser.addFriend(otherUser );
+        
+        List<Trip> trips = service.getTripsByUser(someUser);
+        assertEquals(NO_TRIPS, trips);
+    }
+    
+    @Test public void 
+    we_get_no_trips_if_the_user_hasnt_traveled() throws Exception {
+        doReturn(LOGGED_USER).when(service).getLoggedUser();
+        User someUser = new User();
+        List<Trip> allTrips = asList(TRIP_TO_BAHAMAS, TRIP_TO_LONDON);
+        doReturn(allTrips).when(service).findTripsByUser(someUser);
+        someUser.addFriend(LOGGED_USER);
+        someUser.addTrip(TRIP_TO_BAHAMAS);
+        someUser.addTrip(TRIP_TO_LONDON);
+        
+        List<Trip> trips = service.getTripsByUser(someUser);
+        assertEquals(allTrips, trips);;
+    }
+    
+    
 	
 }
