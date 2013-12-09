2578a0b155b3501489898e88ae8a031b39c44c92
Johan Martinsson <martinsson.johan@gmail.com>
2013-12-10 00:06:00 +0100

    finished refactoring inside TripService

--- java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -10,24 +10,19 @@ import org.craftedsw.tripservicekata.user.UserSession;
 public class TripService {
 
 	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
-		List<Trip> tripList = new ArrayList<Trip>();
-		User loggedUser = getLoggedUser();
-		boolean isFriend = false;
-		if (loggedUser != null) {
-			for (User friend : user.getFriends()) {
-				if (friend.equals(loggedUser)) {
-					isFriend = true;
-					break;
-				}
-			}
-			if (isFriend) {
-				tripList = findTripsByUser(user);
-			}
-			return tripList;
-		} else {
-			throw new UserNotLoggedInException();
-		}
+		assertNotGuest(); 
+		return user.isFriendsWith(getLoggedUser()) ?
+		        findTripsByUser(user) : noTrips();
 	}
+
+    private ArrayList<Trip> noTrips() {
+        return new ArrayList<Trip>();
+    }
+
+    private void assertNotGuest() {
+        if (getLoggedUser() == null)
+            throw new UserNotLoggedInException();
+    }
 
     protected List<Trip> findTripsByUser(User user) {
         return TripDAO.findTripsByUser(user);
