ffb042e4a295c55d9f6933322619d56b4b070f35
Johan Martinsson <martinsson.johan@gmail.com>
2013-12-09 23:19:01 +0100

    first test

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,22 @@
 package org.craftedsw.tripservicekata.trip;
 
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
+import org.craftedsw.tripservicekata.user.User;
+import org.junit.Test;
+
 public class TripServiceTest {
+    
+    protected static final User GUEST = null;
+
+    @Test(expected=UserNotLoggedInException.class)
+    public void fails_for_guests() throws Exception {
+        TripService tripService = new TripService() {
+            @Override
+            protected User getLoggedUser() {
+                return GUEST;
+            }
+        };
+        tripService.getTripsByUser(null);
+    }
 	
 }
