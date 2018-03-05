9cd99fd871c42ba2d30aa4a76d3427bbbae62996
Johan Martinsson <j.martinsson@bee-buzziness.com>
2018-03-05 23:05:18 +0100

    php totally tested 7.1

--- php/src/TripServiceKata/Trip/TripService.php
+++ php/src/TripServiceKata/Trip/TripService.php
@@ -10,8 +10,20 @@ use TripServiceKata\Exception\UserNotLoggedInException;
 class TripService
 {
     public function getTripsByUser(User $user) {
-        $tripList = array();
         $loggedUser = UserSession::getInstance()->getLoggedUser();
+        return $this->_getTripsByUser($user, $loggedUser);
+    }
+
+    /**
+     * @param User $user
+     * @param $loggedUser
+     * @return array|void
+     * @throws UserNotLoggedInException
+     * @throws \TripServiceKata\Exception\DependentClassCalledDuringUnitTestException
+     */
+    public function _getTripsByUser(User $user, $loggedUser)
+    {
+        $tripList = array();
         $isFriend = false;
         if ($loggedUser != null) {
             foreach ($user->getFriends() as $friend) {
@@ -21,12 +33,17 @@ class TripService
                 }
             }
             if ($isFriend) {
-                $tripList = TripDAO::findTripsByUser($user);
+                $tripList = $this->findTripsBy($user);
             }
             return $tripList;
         } else {
             throw new UserNotLoggedInException();
         }
+    }
+
+    protected function findTripsBy(User $user)
+    {
+        return TripDAO::findTripsByUser($user);
     }
 
 }
--- php/test/Test/TripServiceKata/Trip/TripServiceTest.php
+++ php/test/Test/TripServiceKata/Trip/TripServiceTest.php
@@ -3,11 +3,75 @@
 namespace Test\TripServiceKata\Trip;
 
 use PHPUnit\Framework\TestCase;
+use TripServiceKata\Exception\UserNotLoggedInException;
+use TripServiceKata\Trip\Trip;
 use TripServiceKata\Trip\TripService;
+use TripServiceKata\User\User;
 
 class TripServiceTest extends TestCase {
 
+    /** @test
+     * @expectedException TripServiceKata\Exception\UserNotLoggedInException
+     */
+    public function validatesTheUserIsNotGuest()
+    {
+        $tripService = new TripService();
+        $trips = $tripService->_getTripsByUser(new User('Jenny'), null);
+    }
 
+    /** @test */
+    public function showsNoTripsWhenUsersAreStrangers()
+    {
+        $tripService = new TripService();
+        $traveler = new User('traveler');
+        $currentUser = new User('currentUser');
+        $trips = $tripService->_getTripsByUser($traveler, $currentUser);
+
+        $this->assertEquals($this->noTrips(), $trips);
+    }
+
+    /** @test */
+    public function showsTripsToFriends()
+    {
+        $crete = new Trip();
+        $amsterdam = new Trip();
+        $expectedTrips = [$amsterdam, $crete];
+        // given
+        $tripService = new TestableTripService($expectedTrips);
+
+        $traveler = new User('traveler');
+        $currentUser = new User('currentUser');
+        $traveler->addFriend($currentUser);
+
+
+        // when
+        $trips = $tripService->_getTripsByUser($traveler, $currentUser);
+
+        // then
+        $this->assertEquals($expectedTrips, $trips);
+    }
+
+    private function noTrips()
+    {
+        return [];
+    }
+}
+class TestableTripService extends TripService {
+    private $tripsByTraveler;
+
+
+    /**
+     * TestableTripService constructor.
+     */
+    public function __construct($tripsByTraveler)
+    {
+        $this->tripsByTraveler = $tripsByTraveler;
+    }
+
+    protected function findTripsBy(User $user)
+    {
+        return $this->tripsByTraveler;
+    }
 
 }
 
